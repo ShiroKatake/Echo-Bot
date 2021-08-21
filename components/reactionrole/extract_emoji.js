3753dbbc878e80029c940f27387aa3073be8f44a
Khang Trinh <54655740+ShiroKatake@users.noreply.github.com>
2021-08-21 10:12:19 +0000

    Improved error checking

--- components/reactionrole/extract_emoji.js
+++ components/reactionrole/extract_emoji.js
@@ -1,49 +1,78 @@
+const unicodeEmojiRegex = require('emoji-regex/RGI_Emoji.js');
+//TODO: Write error messages and change return statements into reply statements.
 module.exports = function HandleArguments(args, erDictionary) {
   erDictionary = {};
-  let emojis = [];
-  let roles = [];
 
-  for (var i = 0; i < args.length; i++) {
-    let role = ExtractRole(args[i]);
-    if (!role) {
-      emojis.push(ExtractEmoji(args[i]));
+  //Get role from first arg.
+  let role = ExtractRole(args[0]);
+  let roleArgPosition = 0;
+
+  //If the first arg is not a role, return error.
+  if (!role) return;
+
+
+  for (var i = 1; i < args.length; i++) {
+    //If there's anything before or after the role or reaction, return error.
+    if (!SanityCheck(args[i])) return;
+    //Get role from second arg onwards.
+    let argRole = ExtractRole(args[i]);
+
+    if (argRole) {
+      //If there are 2 consecutive args that are roles, return error.
+      if (i - roleArgPosition == 1) return;
+      role = argRole;
+      roleArgPosition = i;
+      continue;
     }
-    else {
-      roles.push(role);
+    else if (!argRole) {
+      let reaction = ExtractReaction(args[i]);
+      //If arg is neither role or reaction, return error.
+      if (!reaction) return;
+      //If a reaction is already used for a role, return error.
+      if (erDictionary[reaction]) return;
+      erDictionary[`${reaction}`] = role;
+      console.log(erDictionary);
     }
-  }
-
-  if (roles.length == 0 || emojis.length == 0) {
-    //Error: Can't be 0
-  }
-  else if (roles.length > 1 && roles.length == emojis.length) {
-    //Error: there must be an emoji for every role
-  }
-
-  for (var i = 0; i < emojis.length; i++) {
-    erDictionary[`${emojis[i]}`] = roles[i] ? roles[i] : roles[0];
   }
 
   return erDictionary;
 }
 
-///Returns the role
-function ExtractRole(arg) {
-  let role = arg.match(/<@&\d+>/g);
+function SanityCheck(str) {
+  return !str.match(/>.|.</g);
+}
+
+function ExtractRole(str) {
+  let role = str.match(/<@&\d+>/g);
   return role ? role[0].match(/\d+/g) : null;
 }
 
-//TODO: Figure out how to check between a unicode character and null. Cuz right now
-//if the arg doesn't fit the regex we just assume that it is a unicode emoji.
+function ExtractReaction(str) {
+  let customEmoji = str.match(/<:.+?:\d+>/g);
 
-function ExtractEmoji(arg) {
-  //If the emoji is unicode, push it as is.
-  let emoji = arg.match(/<:.+?:\d+>/g);
-  if (!emoji) {
-    return arg;
+  //If the emoji is not custom
+  if (!customEmoji) {
+    //Check if it's a unicode symbol and return it.
+    let unicodeEmoji = IsUnicode(str);
+    if (unicodeEmoji)
+      return unicodeEmoji;
+    //Else return error.
+    else
+      return null;
   }
-
-  //If the emoji is custom, push its id instead.
-  let emojiId = emoji[0].match(/\d+/g);
+  
+  //If the emoji is custom, return its id instead.
+  let emojiId = customEmoji[0].match(/\d+/g);
   return emojiId;
+}
+
+function IsUnicode(str) {
+  result = str.match(unicodeEmojiRegex());
+  //If there's more than 1 occurences (ie. there are at least 2 unicode emojis being next to together without space)
+  //or if it's a unicode but not an emoji,
+  //return error.
+  if (!result || !result.length == 1)
+    return false; //TODO: Change this to reply error message.
+
+  return result;
 }
--- components/reactionrole/on_reaction_remove.js
+++ components/reactionrole/on_reaction_remove.js
@@ -1,38 +1,38 @@
 module.exports = async (reaction, user, message, botMessage, erDictionary) => {
   //If the user reacted to a bot message in the past, disregard it.
-    if (reaction.message.id != botMessage.author.lastMessageID) return;
-    if (reaction.message.partial) await reaction.message.fetch();
-    if (reaction.partial) await reaction.fetch();
-    if (user.bot) return;
-    if (!reaction.message.guild) return;
-    
-    if (reaction.message.channel.id == message.channel.id) {
-      const guildMember = reaction.message.guild.members.cache.get(user.id);
-      const messageReactions = reaction.message.reactions.cache;
+  if (reaction.message.id != botMessage.author.lastMessageID) return;
+  if (reaction.message.partial) await reaction.message.fetch();
+  if (reaction.partial) await reaction.fetch();
+  if (user.bot) return;
+  if (!reaction.message.guild) return;
+  
+  if (reaction.message.channel.id == message.channel.id) {
+    const guildMember = reaction.message.guild.members.cache.get(user.id);
+    const messageReactions = reaction.message.reactions.cache;
 
-      let reactionRoleToBeRemoved = GetRoleFromReaction(reaction, erDictionary);
-      if(!reactionRoleToBeRemoved) return;
+    let reactionRoleToBeRemoved = GetRoleFromReaction(reaction, erDictionary);
+    if(!reactionRoleToBeRemoved) return;
 
-      //For each reaction the bot put on the message,
-      //check to see if the user reacted to anything else.
-      const memberReactions = messageReactions.filter(reaction => reaction.users.cache.has(user.id)).values();
-      let reactedRoles = [];
-      for (const reaction of memberReactions) {
-        reactedRoles.push(GetRoleFromReaction(reaction, erDictionary));
-      };
+    //For each reaction the bot put on the message,
+    //check to see if the user reacted to anything else.
+    const memberReactions = messageReactions.filter(reaction => reaction.users.cache.has(user.id)).values();
+    let reactedRoles = [];
+    for (const reaction of memberReactions) {
+      reactedRoles.push(GetRoleFromReaction(reaction, erDictionary));
+    };
 
-      //If the user reacted to many emojis that assign the same role,
-      //don't remove that role until there's only one left.
-      let roleHasManyOccurences = false;
-      reactedRoles.forEach(reactedRole => {
-        if (reactedRole == reactionRoleToBeRemoved) roleHasManyOccurences = true;
-      });
-      if (roleHasManyOccurences) return;
+    //If the user reacted to many emojis that assign the same role,
+    //don't remove that role until there's only one left.
+    let roleHasManyOccurences = false;
+    reactedRoles.forEach(reactedRole => {
+      if (reactedRole == reactionRoleToBeRemoved) roleHasManyOccurences = true;
+    });
+    if (roleHasManyOccurences) return;
 
-      //TODO: Handle errors properly.
-      await guildMember.roles.remove(`${reactionRoleToBeRemoved}`);
-    }
-    return;
+    //TODO: Handle errors properly.
+    await guildMember.roles.remove(`${reactionRoleToBeRemoved}`);
+  }
+  return;
 }
 
 function GetRoleFromReaction(reaction, dictionary) {
